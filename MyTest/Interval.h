61e9b370c07a487c0dcf817b51c28b564390ef74
Yizheng Cai <yizhengc@gmail.com>
2012-03-13 08:41:24 -0700

    Merge Interval

--- MyTest/Interval.h
+++ MyTest/Interval.h
@@ -47,3 +47,59 @@ int LargestOverlap(Interval* intervals, int length, int& idx1, int& idx2)
 
 	return maxOverlap;
 }
+
+int MergeIntervals(Interval* range, int length)
+{
+	int j = 0;
+	for(int i = 1; i < length; i++)
+	{
+		if (range[i].LB <= range[j].RB)
+		{
+			range[j].RB = max(range[i].RB, range[j].RB);
+		}
+		else
+		{
+			j++;
+			range[j].LB = range[i].LB;
+			range[j].RB = range[i].RB;
+		}
+	}
+
+	return j + 1;
+}
+
+void TestLargestOverlapIntervals()
+{
+	int ranges[7][2] = { {1, 6}, {3, 9}, {4, 6}, {5, 9}, {9, 13}, {11, 19}, {12, 18}};
+
+	Interval* intervals = new Interval[7];
+
+	for(int i = 0; i < 7; i++)
+	{
+		intervals[i].LB = ranges[i][0];
+		intervals[i].RB = ranges[i][1];
+	}
+
+	int idx1, idx2;
+	int overlap = LargestOverlap(intervals, 7, idx1, idx2);
+}
+
+void TestMergeInterval()
+{
+	int ranges[7][2] = { {1, 6}, {3, 9}, {4, 6}, {5, 9}, {9, 10}, {11, 19}, {12, 18}};
+
+	Interval* intervals = new Interval[7];
+
+	for(int i = 0; i < 7; i++)
+	{
+		intervals[i].LB = ranges[i][0];
+		intervals[i].RB = ranges[i][1];
+	}
+
+	int len = MergeIntervals(intervals, 7);
+
+	for(int i = 0; i < len; i++)
+	{
+		cout << intervals[i].LB << " " << intervals[i].RB << endl;
+	}
+}
