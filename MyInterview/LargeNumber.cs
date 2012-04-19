491162215bdfe9900aef0adb0c22f3566ec08ece
Yizheng Cai <yizhengc@gmail.com>
2012-04-18 18:31:20 -0700

    RegexMatch and largeNumber math

---
+++ MyInterview/LargeNumber.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyInterview
+{
+    class LargeNumber
+    {
+        static string Multiply(string a, string b)
+        {
+            int[] result = new int[a.Length + b.Length];
+            int carry = 0;
+
+            for (int i = 0; i < b.Length; i++)
+            {
+                int offset = 0;
+                for (int j = 0; j < a.Length; j++)
+                {
+                    offset = result.Length - 1 - (j + i);
+                    int va = c2a(a[a.Length - 1 - j]);
+                    int vb = c2a(b[b.Length - 1 - i]);
+
+                    int temp = va * vb + result[offset] + carry;
+
+                    if (temp >= 10)
+                    {
+                        result[offset] = temp % 10;
+                        carry = temp / 10;
+                    }
+                    else
+                    {
+                        result[offset] = temp;
+                        carry = 0;
+                    }
+                }
+
+                result[offset - 1] = carry;
+                carry = 0;
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            bool leadingZero = true;
+            foreach (int i in result)
+            {
+                if (i == 0 && leadingZero)
+                    continue;
+                else
+                {
+                    leadingZero = false;
+                    sb.Append(i);
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        static int c2a(char c)
+        {
+            return c - '0';
+        }
+
+        public static void UnitTest()
+        {
+            Random rand = new Random();
+            int n = 0;
+            while (n < 100)
+            {
+                int a = rand.Next(10000);
+                int b = rand.Next(10000);
+
+                string result = (a * b).ToString();
+
+                string actual =  Multiply(a.ToString(), b.ToString());
+
+                if(actual != result)
+                    Console.WriteLine("MissMatch");
+
+                n++;
+            }
+        }
+    }
+}
---
+++ MyInterview/RegexMatch.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyInterview
+{
+    class RegexMatch
+    {
+        static bool Match(string word, int start, string regex, int rstart)
+        {
+            if (start == word.Length && rstart == regex.Length)
+                return true;
+
+            if (start != word.Length && rstart == regex.Length)
+                return false;
+
+            if (rstart == regex.Length - 1 || !IsSpecialChar(regex[rstart+1]))
+            {
+                if (start == word.Length)
+                    return false;
+
+                if (IsSpecialChar(regex[rstart]))
+                    throw new Exception();
+                else if (word[start] != regex[rstart])
+                    return false;
+                else
+                    return Match(word, start + 1, regex, rstart + 1);
+            }
+            else
+            {
+                char specialChar = regex[rstart + 1];
+                char c = regex[rstart];
+
+                if (specialChar == '?')
+                {
+                    if (start < word.Length && word[start] == c)
+                        return Match(word, start + 1, regex, rstart + 2);
+                    else
+                        return Match(word, start, regex, rstart + 2);
+                }
+                else if (specialChar == '*')
+                {
+                    if (start < word.Length && word[start] == c)
+                    {
+                        return Match(word, start, regex, rstart + 2) || Match(word, start + 1, regex, rstart) || Match(word, start + 1, regex, rstart + 2);
+                    }
+                    else
+                        return Match(word, start, regex, rstart + 2);
+                }
+                else
+                    throw new Exception();
+            }
+        }
+
+        static bool IsSpecialChar(char c)
+        {
+            return c == '*' || c == '?';
+        }
+
+        public static void UnitTest()
+        {
+            string regex = "a*b?c?";
+
+            string[] str = new string[] { "", "abbc", "ac", "bc", "c", "ab", "ba" };
+
+            foreach (var s in str)
+            {
+                Console.WriteLine("{0}:{1}", s, Match(s, 0, regex, 0));
+            }
+        }
+    }
+}
